1ccf961fef5492f737cfcd83a64d682730980b03
boby <boby@eprotime.com>
2015-03-30 18:19:35 +0800

    update database.js server.js

--- server.js
+++ server.js
@@ -7,11 +7,19 @@ var path = require('path');
 var margan = require('morgan');
 //mongodb driver
 var mongoskin = require('mongoskin');
-
+var database = require('./config/db');
+var db = mongoskin.db(database.url,{native_parser:true});
 
 var port = process.env.PORT || 8088;
 //serve static directory 
 app.use(exress.static(path.join(__dirname,'public')));
+app.use(margan('dev'));
+//parse request bodies (req.body)
+app.use(bodyParser.urlencoded({'extended':'true'}))
+//allow overriding methods in query (?_method=put)
+app.use(methodOverride('_method'));
+
+
 
 
 app.listen(port);
