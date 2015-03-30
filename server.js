8334e1f9526a4737719fe0487d0e7970b49ccd1f
boby <boby@eprotime.com>
2015-03-30 18:01:34 +0800

    update server.js listen 8088

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 8088；
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var path = require('path');
+//logs
 var margan = require('morgan');
-
+//mongodb drive
 var mongoskin = require('mongoskin');
+
+
+var port = process.env.PORT || 8088;
+//serve static directory 
+app.use(exress.static(path.join(__dirname,'public')));
+
+
+app.listen(port);
+console.log('my app listen port ',port);
